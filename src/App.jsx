72e05ed62aa1a47f4c99af7fbd7b996a0254807f
Cham2 <dearcham2@gmail.com>
2024-05-28 11:35:16 +0900

    ☕️ cream cream studio로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import EffectElement from './EffectElement';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   const [left, setLeft] = useState(0);
   const [top, setTop] = useState(0);
   const [isHovered, setIsHovered] = useState(false);
@@ -29,29 +25,12 @@ function App() {
         style={{ left, top }}
         className={isHovered ? 'hovered' : ''}
       />
-
       <EffectElement setIsHovered={setIsHovered}>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div id="title">
+          <p>Cream Cream</p>
+          <strong>Studio</strong>
+        </div>
       </EffectElement>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <EffectElement setIsHovered={setIsHovered}>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-        </EffectElement>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
